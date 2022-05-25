46c3a4915c5c1361724b4d29a606f3304a0feaca
Thilanka0824 <frontleanrest24@gmail.com>
2022-05-25 11:53:55 -0700

    all complete

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const prompt = require('prompt-sync')({sigint: true});
 
+/*
+
 let choose = Number(prompt("Select 1 to combine colors or 2 to deconstruct a color, then press ENTER: "))
 
 if (choose === 1){
@@ -33,3 +35,27 @@ if (choose === 1){
 }
 
 
+*/
+
+const str = prompt("pick a color or two: ")
+
+// =====================CONSTRUCT========================
+if (str.includes("purple")){
+    console.log("purple is made by combining RED and BLUE")
+} else if (str.includes("orange")){
+    console.log("orange is made by combining RED and YELLOW")
+} else if (str.includes("green")){
+    console.log("green is made by combining BLUE and YELLOW")
+}
+
+// =====================DECONSTRUCT=====================
+
+else if (str.includes("red blue") || str.includes("blue red")){ 
+    console.log("red and blue combined make PURPLE") // this happens if 2 colors are picked
+} else if (str.includes("red yellow") || str.includes("yellow red")){
+    console.log("red and yellow combined make ORANGE")
+} else if (str.includes("blue yellow") || str.includes("yellow blue")){
+    console.log("blue and yellow combined make GREEN")
+} else {
+    console.log("error")
+}
